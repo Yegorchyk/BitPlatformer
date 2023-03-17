5d23e1ccc49df9277a3383dfbc43af91547d17e2
Yegorchyk <127146240+Yegorchyk@users.noreply.github.com>
2023-03-18 00:05:37 +0200

    Hueta

--- Assets/Scripts/Enemyes/PatrulEnemy.cs
+++ Assets/Scripts/Enemyes/PatrulEnemy.cs
@@ -5,42 +5,71 @@ using UnityEngine;
 
 public class PatrulEnemy : MonoBehaviour
 {
-    public int vecNum, doorNum;
+    public int vecNum, lastNum, doorNum;
     public Vector2[] vect;
     public Vector2[] doors;
     public float speed;
-
+    public bool doormove;
     
     void Start()
     {
         vecNum = Random.Range(0, vect.Length);
+
+        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(vecNum);
+        //Debug.Log(transform.position.y % doors[doorNum].y);
        
 
-        if(transform.position.y - vect[vecNum].y > 1)
+        if(vect[lastNum].y != vect[vecNum].y)
         {
+            Debug.Log("Yes");
+            doormove = true;
             transform.position = Vector2.MoveTowards( transform.position, doors[doorNum], speed);
         }
-        else
+        else /*if(transform.position.y % doors[doorNum].y < 0.05f && transform.position.y % doors[doorNum].y > -0.1f)*/
         {
+            Debug.Log("No");
             transform.position = Vector2.MoveTowards(transform.position, vect[vecNum], speed);
         }
 
         if(Vector2.Distance(transform.position, vect[vecNum]) <= 0.1)
         {
+            lastNum = vecNum;
             vecNum = Random.Range(0, vect.Length);
         }
 
-        if(Vector2.Distance(transform.position, doors[doorNum]) <= 0.1)
+        if(Vector2.Distance(transform.position, doors[doorNum]) <= 0.1 && doormove == true)
         {
-            if(vecNum < 2)
+            switch (doorNum)
             {
-                transform.position = doors[2];
+                case 0:
+                    doorNum = 1;
+                    doormove = false;
+                    transform.position = doors[1];
+                   
+
+                    break;
+
+                case 1:
+                    doorNum = 2;
+                    doormove = false;
+                    transform.position = doors[2];
+                
+
+                    break;
+
+                case 2:
+
+                    doorNum = 0;
+                    doormove = false;
+                    transform.position = doors[0];
+                    
+                    break;
             }
         }
     }
