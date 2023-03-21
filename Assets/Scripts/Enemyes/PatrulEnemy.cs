60b04959ab8c8ae5d82618568a725794699a244b
Yegorchyk <127146240+Yegorchyk@users.noreply.github.com>
2023-03-21 22:49:55 +0200

    suka

--- Assets/Scripts/Enemyes/PatrulEnemy.cs
+++ Assets/Scripts/Enemyes/PatrulEnemy.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -14,27 +12,41 @@ public class PatrulEnemy : MonoBehaviour
     void Start()
     {
         vecNum = Random.Range(0, vect.Length);
+        lastNum = 4;
 
-        
-      
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(transform.position.y % doors[doorNum].y);
-       
+        Debug.Log(doormove);
 
-        if(vect[lastNum].y != vect[vecNum].y)
+        if (doormove == true)
         {
-            Debug.Log("Yes");
+            transform.position = Vector2.MoveTowards(transform.position, doors[doorNum], speed);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, vect[vecNum], speed);
+        }
+
+        if (transform.position.y - vect[vecNum].y > 0.1f || transform.position.y - vect[vecNum].y < -0.1f)
+        {
+            Debug.Log(transform.position.y - vect[vecNum].y);
+          
             doormove = true;
-            transform.position = Vector2.MoveTowards( transform.position, doors[doorNum], speed);
+           
+
+           
         }
         else /*if(transform.position.y % doors[doorNum].y < 0.05f && transform.position.y % doors[doorNum].y > -0.1f)*/
         {
-            Debug.Log("No");
-            transform.position = Vector2.MoveTowards(transform.position, vect[vecNum], speed);
+            //Debug.Log("No");
+            //lastNum = vecNum;
+            doormove = false;
+            
         }
 
         if(Vector2.Distance(transform.position, vect[vecNum]) <= 0.1)
@@ -49,7 +61,7 @@ public class PatrulEnemy : MonoBehaviour
             {
                 case 0:
                     doorNum = 1;
-                    doormove = false;
+                  
                     transform.position = doors[1];
                    
 
@@ -57,7 +69,7 @@ public class PatrulEnemy : MonoBehaviour
 
                 case 1:
                     doorNum = 2;
-                    doormove = false;
+                   
                     transform.position = doors[2];
                 
 
@@ -66,11 +78,13 @@ public class PatrulEnemy : MonoBehaviour
                 case 2:
 
                     doorNum = 0;
-                    doormove = false;
+                 
                     transform.position = doors[0];
                     
                     break;
             }
         }
+
+       
     }
 }
